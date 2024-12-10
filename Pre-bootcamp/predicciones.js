f7d0bd9e9f2ed32e517a014b63ac00e88fcac75b
Cynthia Castillo <ca.castillo@gmail.com>
2024-12-10 10:28:51 -0600

    Commit inicial

---
+++ Pre-bootcamp/predicciones.js
@@ -0,0 +1,35 @@
+// function sumatoria(numero1, numero2) {
+
+//     return numero1 + numero2;
+ 
+//  }
+ 
+//  console.log(sumatoria(4, 3));
+ 
+//  console.log(sumatoria(2, 9));
+
+
+ /*
+ var     valor
+ IMPRIMIR ?
+ sumatoria(4,3)
+ numero1 = 4
+ numero2 = 3
+ <- 4+3
+ IMPRIMIR 7
+
+ IMPRIMIR ?
+ sumatoria(2, 9)
+ numero1 = 2
+ numero2 = 9
+ <- 2+9
+ IMPRIMIR 11
+ 
+ */
+
+var edad = 25; // "global" 
+function miEdad(edad) { //edad = 25
+    console.log("Tengo: " + edad + " años"); // Tengo 25 años
+}
+
+miEdad(edad); //miEdad(25)

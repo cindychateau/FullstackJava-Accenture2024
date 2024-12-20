c528c214dbb82e48b634f6612d2bef67e64a4ab9
Cynthia Castillo <ca.castillo@gmail.com>
2024-12-20 17:16:28 -0600

    Query Selector

--- Fundamentos de la Web/5.- Javascript/profile-page/js/script.js
+++ Fundamentos de la Web/5.- Javascript/profile-page/js/script.js
@@ -29,3 +29,39 @@ function cambia_imagen(elemento_img) {
 function regresa_imagen(elemento_img) {
     elemento_img.src = "images/jane-m.jpg";
 }
+
+function editar_perfil() {
+    let elemento_nombre = document.querySelector("#nombre");
+    let elemento_parrafo = document.querySelector(".descripcion");
+
+    elemento_nombre.innerText = "Cynthia Castillo";
+    elemento_parrafo.innerHTML = "Instructora de programación <br> Amante de los gatitos.";
+
+    //color: white;
+    elemento_nombre.style.color = "white";
+    //background-color: babyblue;
+    elemento_nombre.style.backgroundColor = "blue";
+    //camelCase PascalCase
+
+}
+
+function busqueda() {
+    let input = document.querySelector("#buscador");
+    let valor = input.value; //Valor que el usuario escribió en el input
+    alert("Estás buscando: "+valor);
+}
+
+function cambiar_modo() {
+    let tarjeta_usuario = document.querySelector(".user-card");
+
+    //tarjeta_usuario.classList.add("dark-mode");
+
+    if(tarjeta_usuario.classList.contains("dark-mode")) {
+        tarjeta_usuario.classList.remove("dark-mode");
+    } else {
+        tarjeta_usuario.classList.add("dark-mode");
+    }
+
+
+}
+

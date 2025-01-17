8e05341bea9ced6ac5d9cdcd38a56b6a889b6eb9
Cynthia Castillo <ca.castillo@gmail.com>
2025-01-17 15:58:26 -0600

    Intro STS

---
+++ Java/3.- STS/Herencia/src/com/codingdojo/cynthia/Aplicacion.java
@@ -0,0 +1,12 @@
+package com.codingdojo.cynthia;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		
+		Vehiculo auto1 = new Vehiculo("rojo");
+		System.out.println("El color del auto es:"+auto1.getColor());
+		
+	}
+
+}
